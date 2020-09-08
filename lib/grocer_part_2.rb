c35ef39668d8e30c3a8667f3e0433a1b95e6bc83
Mike Smith <gooseisdead@mac.com>
2020-09-08 17:28:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -28,9 +28,13 @@ end
 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < cart.length
+    if cart[counter][:clearance] == true
+      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
+    end
+    counter += 1
+  end
 end
 
 def checkout(cart, coupons)
